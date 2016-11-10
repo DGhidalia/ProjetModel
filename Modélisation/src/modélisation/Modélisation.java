b89a60c2b4d1b57ebdd4622738062d01da883e9e
DGhidalia <david.ghidalia@outlook.fr>
2016-11-10 15:53:40 +0100

    Ajout projet Vide
    Projet Vide Java Modélisation

---
+++ Modélisation/src/modélisation/Modélisation.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modélisation;
+
+/**
+ *
+ * @author dg738399
+ */
+public class Modélisation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
